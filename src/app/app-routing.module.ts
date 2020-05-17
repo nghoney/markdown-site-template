9409e730a2843ec39dbc8f85215aa90c946e0f39
Edward <crazybber@outlook.com>
2020-05-17 23:29:33 +0800

    add  material design theme
    https://material.angular.io/guide/getting-started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,16 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
-  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.LoginComponent) },
-  { path: 'updates', loadChildren: () => import('./updates/updates.module').then(m => m.UpdateLogsComponent) },
-  {
-    path: 'initializr',
-    loadChildren: () => import('./initializr/initializr.module').then(m => m.InitializrModule),
-    canActivate: [ AuthService ]
-  },
+  // { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
+  // { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  // { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  // { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.LoginComponent) },
+  // { path: 'updates', loadChildren: () => import('./updates/updates.module').then(m => m.UpdateLogsComponent) },
+  // {
+  //   path: 'initializr',
+  //   loadChildren: () => import('./initializr/initializr.module').then(m => m.InitializrModule),
+  //   canActivate: [ AuthService ]
+  // },
 ];
 
 @NgModule({
