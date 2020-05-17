54cd3b6278402b7fc20c09536084fb9bd20cf941
Edward <crazybber@outlook.com>
2020-05-17 22:05:34 +0800

    rename markdown-docs ==>markdown-site-template

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('markdown-docs app is running!');
+    expect(page.getTitleText()).toEqual('markdown-site-template app is running!');
   });
 
   afterEach(async () => {
