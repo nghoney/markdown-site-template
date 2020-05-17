7cdd75521c156aad4f1bfbfddf8b995ff6ec0103
Edward <crazybber@outlook.com>
2020-05-17 23:11:43 +0800

    update projects config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'updates', component: UpdateLogsComponent },
+  {
+    path: 'initializr',
+    loadChildren: 'app/initializr/initializr.module#InitializrModule',
+    canActivate: [ AuthService ]
+  },
+  {
+    path: '',
+    loadChildren: 'app/blog/blog.module#BlogModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
