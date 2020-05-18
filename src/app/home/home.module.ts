66e93ebddc3f571d8a6a1a9e1d7a3af9383f9a81
Edward <crazybber@outlook.com>
2020-05-18 11:33:38 +0800

    update home.component relationship

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home.component';
 
 
 @NgModule({
