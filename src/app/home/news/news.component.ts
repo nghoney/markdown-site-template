4ca590f16edb8f97dafbc629e81697a15976fdd3
Edward <crazybber@outlook.com>
2020-05-20 23:30:04 +0800

    add a news components

--- src/app/home/news/news.component.ts
+++ src/app/home/news/news.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PageEvent, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  repo: any;
+  issues: Observable<any[]>;
+  length;
+  pageSize = 7;
+  pageSizeOptions = [7];
 
-  ngOnInit(): void {
+  pageEvent: PageEvent;
+
+  constructor(
+    private matPaginatorIntl: MatPaginatorIntl,
+    public snackBar: MatSnackBar,
+   // private confService: ConfService,
+   // private gitProxyService: GitProxyService
+  ) { }
+
+  ngOnInit() {
+    this.matPaginatorIntl.itemsPerPageLabel = "count per page：";
+    this.matPaginatorIntl.nextPageLabel = "";
+    this.matPaginatorIntl.previousPageLabel = "";
+    this.matPaginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number) => { if (length == 0 || pageSize == 0) { return `0 of ${length}`; } length = Math.max(length, 0); const startIndex = page * pageSize;
+      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize; return `${startIndex + 1} - ${endIndex} / ${length}`; }
+   
+  }
+
+  ngAfterViewInit() {
+    this.search();
+  }
+
+  search(): void {
+    this.matPaginatorIntl.changes.next();
+  }
+
+  onClosed(isClosed: boolean) {
+    this.search();
+    this.snackBar.open("done ! ", "", {
+      duration: 600
+    });
   }
 
 }
