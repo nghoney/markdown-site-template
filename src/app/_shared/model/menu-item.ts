38a170589c27686727d21159adbdd277aff29874
Edward <crazybber@outlook.com>
2020-05-24 21:46:31 +0800

    add type model for menu-item catalog

---
+++ src/app/_shared/model/menu-item.ts
@@ -0,0 +1,12 @@
+export class LeftMenuCatalogItem {
+    color: string; //"DB2828"
+    created_at: string; //"2020-05-18T20:06:42+08:00"
+    id: number;//64302304
+    name: string; //"bug"
+    repository_id: number; //9420731
+    updated_at: string; //"2020-05-18T20:06:42+08:00"
+    url: string; //"https://gitee.com/api/v5/repos/crazybber/markdown-site-template/labels/bug"
+}
+
+
+
