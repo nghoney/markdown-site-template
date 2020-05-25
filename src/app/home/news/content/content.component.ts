b0725e413abd53a349f1094aa0b6738e46bdeb56
Edward <crazybber@outlook.com>
2020-05-25 18:17:42 +0800

    add layout desing for content.component

--- src/app/home/news/content/content.component.ts
+++ src/app/home/news/content/content.component.ts
@@ -1,11 +1,20 @@
+/*
+ * @Author: Edward https://github.com/crazybber
+ * @Date: 2020-05-21 21:46:48
+ * @LastEditors: Edward
+ * @FilePath: \markdown-site-template\src\app\home\news\content\content.component.ts
+ * @LastEditTime: 2020-05-25 18:00:56
+ * @description: NG markdown site template @ MIT License
+ */
+
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ConfigService } from 'src/app/_shared/service/config.service';
-import { AuthService } from 'src/app/_shared/service/auth.service';
+import { ConfigService, AuthService, GitService, UtilsService } from '../../../_shared/service';
+import { CloseDialogComponent } from '../close-dialog/close-dialog.component';
 
 @Component({
-  selector: 'app-content',
+  selector: 'app-content-item',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
@@ -13,7 +22,7 @@ export class ContentComponent implements OnInit {
 
   @Input() issue: any;
   @Output() onClosed = new EventEmitter<boolean>();
-  headerImageStyle = { };
+  headerImageStyle = {};
   user: string;
   createAt: string;
   title: string;
@@ -22,8 +31,10 @@ export class ContentComponent implements OnInit {
   constructor(
     public dialog: MatDialog,
     public snackBar: MatSnackBar,
-    private confService: ConfigService ,
+    private confService: ConfigService,
     public authService: AuthService,
+    private gitService: GitService,
+    private utilsService: UtilsService
   ) { }
 
   ngOnInit() {
@@ -34,14 +45,35 @@ export class ContentComponent implements OnInit {
     }
     this.createAt = this.issue.created_at;
     this.title = this.issue.title;
+    this.utilsService.markdownText(this.utilsService.emojiParser(this.issue.body))
+      .then(body => this.body = body);
 
   }
 
   openCloseIssueDialog(): void {
+    let dialogRef = this.dialog.open(CloseDialogComponent, {
+      minWidth: '25vw',
+      minHeight: '30vh',
+      data: {
+        title: this.issue.title
+      }
+    });
 
+    dialogRef.afterClosed()
+      .subscribe(result => {
+        if (result) {
+          this.closeIssue();
+        }
+      });
   }
 
   closeIssue(): void {
-
+    this.gitService.updateIssue(
+      this.issue.number,
+      this.issue.title,
+      sessionStorage.getItem('access_token'),
+      'closed'
+    )
+      .then(() => this.onClosed.emit(true));
   }
 }
--- src/app/home/news/news.component.ts
+++ src/app/home/news/news.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+/*
+ * @Author: Edward https://github.com/crazybber
+ * @Date: 2020-05-20 22:32:30
+ * @LastEditors: Edward
+ * @FilePath: \markdown-site-template\src\app\home\news\news.component.ts
+ * @LastEditTime: 2020-05-25 16:54:05
+ * @description: NG markdown site template @ MIT License
+ */
+
+import { GitService } from '../../_shared/service';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { PageEvent, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -8,11 +19,11 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent implements OnInit, AfterViewInit {
 
   repo: any;
   issues: Observable<any[]>;
-  length;
+  length = 0;
   pageSize = 7;
   pageSizeOptions = [7];
 
@@ -21,30 +32,46 @@ export class NewsComponent implements OnInit {
   constructor(
     private matPaginatorIntl: MatPaginatorIntl,
     public snackBar: MatSnackBar,
-   // private confService: ConfigService ,
-   // private gitProxyService: GitService
+    private gitService: GitService
   ) { }
 
   ngOnInit() {
     this.matPaginatorIntl.itemsPerPageLabel = "count per page：";
     this.matPaginatorIntl.nextPageLabel = "";
     this.matPaginatorIntl.previousPageLabel = "";
-    this.matPaginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number) => { if (length == 0 || pageSize == 0) { return `0 of ${length}`; } length = Math.max(length, 0); const startIndex = page * pageSize;
-      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize; return `${startIndex + 1} - ${endIndex} / ${length}`; }
-   
+    this.matPaginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+      if (length == 0 || pageSize == 0) {
+        return `0 of ${length}`;
+      }
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+      return `${startIndex + 1} - ${endIndex} / ${length}`;
+    }
+
+    console.log('init issue list')
+
+    this.gitService.getRepo().then(repo => this.length = repo.open_issues_count);
+
+    this.issues = this.matPaginatorIntl.changes.pipe(switchMap(() => this.gitService.simpleGetIssues(
+      'news',
+      this.pageEvent ? this.pageEvent.pageIndex + 1 : 1,
+      this.pageSize)
+    )
+    );
   }
 
   ngAfterViewInit() {
-    this.search();
+    this.updatePaginatorDisplay();
   }
 
-  search(): void {
+  updatePaginatorDisplay(): void {
     this.matPaginatorIntl.changes.next();
   }
 
   onClosed(isClosed: boolean) {
-    this.search();
-    this.snackBar.open("done ! ", "", {
+    this.updatePaginatorDisplay();
+    this.snackBar.open("deleted finished !", "alert", {
       duration: 600
     });
   }
