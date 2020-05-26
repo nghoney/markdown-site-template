8af0db1ac4bc2796fe8dd8170c6b7b1b13d561aa
eamon <eamon_chen@outlook.com>
2020-05-26 18:40:47 +0800

    add a seperated left-side menu

--- src/app/home/menu/menu.component.ts
+++ src/app/home/menu/menu.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AuthService, UtilsService, ContentService } from '../../_shared/service';
 
 @Component({
-  selector: 'app-menu',
+  selector: 'app-left-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatSidenav)
+  sideNav: MatSidenav;
 
-  ngOnInit(): void {
+  leftMenuCatalogs: any[];
+
+  markedDir: string;
+
+  constructor(
+    private router: Router,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    private contentService: ContentService,
+    public authService: AuthService,
+    private utilsService: UtilsService
+  ) { }
+
+  ngOnInit() {
+    this.contentService
+      .getLeftMenuCatalogs()
+      .then(catalogs => {
+        this.leftMenuCatalogs = catalogs;
+        console.log('left menu catalog:', this.leftMenuCatalogs);
+      });
   }
 
+  getSideNavMode(): string {
+    if (this.utilsService.isSmartDevice()) {
+      return "over";
+    }
+    return "side";
+  }
+
+  isMarkedDir(dir: string): boolean {
+    return dir == this.markedDir;
+  }
+
+  isDefaultOpen(): boolean {
+    return !this.utilsService.isSmartDevice();
+  }
+
+  close(): void {
+    if (this.utilsService.isSmartDevice()) {
+      this.sideNav.close();
+    }
+  }
+
+  openAddBlogDialog(): void {
+
+  }
+
+  createBlog(dir: string, file: string): void {
+
+  }
+
+  handleError(err: HttpErrorResponse): void {
+
+  }
+
+  onDeleted(isDeleted: boolean) {
+
+  }
 }
