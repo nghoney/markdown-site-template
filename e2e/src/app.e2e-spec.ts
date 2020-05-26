22f215056e57034fdf51b10e37079f7e1daf03bd
eamon <eamon_chen@outlook.com>
2020-05-26 10:46:13 +0800

    fix :upper case to lower case

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('markdown-site-template App', () => {
   let page: AppPage;
 
   beforeEach(() => {
